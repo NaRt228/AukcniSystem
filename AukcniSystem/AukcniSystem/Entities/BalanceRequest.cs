4199ee9e76eec5aee370c8b9ad76b9a586fd2bbe
NaRt228 <zakarpatskimulti@gmail.com>
2026-04-29 13:52:58 +0200

    Admin cat and users

---
+++ AukcniSystem/AukcniSystem/Entities/BalanceRequest.cs
@@ -0,0 +1,17 @@
+﻿using AukcniSystem.Data;
+
+namespace AukcniSystem.Entities
+{
+    public class BalanceRequest
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime RequestedAt { get; set; } = DateTime.Now;
+        public bool? IsApproved { get; set; } // null = čeká, true = schváleno, false = zamítnuto
+        public DateTime? HandledAt { get; set; }
+
+        // Relace
+        public string UserId { get; set; } = string.Empty;
+        public ApplicationUser User { get; set; } = null!;
+    }
+}
